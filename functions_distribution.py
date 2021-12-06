d4ce4cbff150700a8490019534d500e08d305721
nielsvanesseveldt <92365187+nielsvanesseveldt@users.noreply.github.com>
2021-12-06 12:56:33 +0100

    Add files via upload

--- functions_distribution.py
+++ functions_distribution.py
@@ -4,12 +4,15 @@
 # In[3]:
 
 
-def get_coords_from_prob_landscape(prob_landscape):
+def get_coords_from_prob_landscape(t,t0,prob_landscape):
     import numpy as np
-    prob_landscape = prob_landscape/(np.sum(prob_landscape))
-    index = np.arange(0,3150)
-    chosenindex = np.random.choice(index,p=prob_landscape.ravel())
-    x_goal, y_goal = chosenindex//150, chosenindex%150
+    if t < t0:
+        prob_landscape = prob_landscape/(np.sum(prob_landscape))
+        index = np.arange(0,3150)
+        chosenindex = np.random.choice(index,p=prob_landscape.ravel())
+        x_goal, y_goal = chosenindex//150, chosenindex%150
+    elif t>= t0:
+        x_goal, y_goal = 1,1
     return x_goal, y_goal
 
 
