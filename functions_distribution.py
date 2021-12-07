ccde58c65a7ea1e29172126c0b8edc01c26424f7
nielsvanesseveldt <92365187+nielsvanesseveldt@users.noreply.github.com>
2021-12-07 13:09:46 +0100

    Add files via upload

--- functions_distribution.py
+++ functions_distribution.py
@@ -4,17 +4,20 @@
 # In[3]:
 
 
-def get_coords_from_prob_landscape(t,t0,prob_landscape):
+def get_coords_from_prob_landscape(n,t,t0,prob_landscape):
     import numpy as np
     if t < t0:
         prob_landscape = prob_landscape/(np.sum(prob_landscape))
         index = np.arange(0,3150)
-        chosenindex = np.random.choice(index,p=prob_landscape.ravel())
+        chosenindex = np.random.choice(index,n,p=prob_landscape.ravel())
         x_goal, y_goal = chosenindex//150, chosenindex%150
     elif t>= t0:
         x_goal, y_goal = 1,1
     return x_goal, y_goal
-
+# n is the amount of random coordinates you want
+# t is the moment you're interested in
+# t0 is the moment when the train arrives
+# prob_landscape is the csv file you need to import
 
 # In[ ]:
 
