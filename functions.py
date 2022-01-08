71de0b9e45d8e0ac09565ba9f55a584566b9d1c3
nielsvanesseveldt <92365187+nielsvanesseveldt@users.noreply.github.com>
2022-01-08 15:05:51 +0100

    Add files via upload

--- functions.py
+++ functions.py
@@ -67,10 +67,11 @@ def get_field(ped_list):
         M = np.zeros([20,150])
         M[ped_list_array[:,1, 0], ped_list_array[:,1, 1]] = -1
         M[ped_list_array[ped,1, 0], ped_list_array[ped,1, 1]] = 0;
+        M[ped_list_array[ped,0, 0], ped_list_array[ped,0, 1]] = 5; #goals, the value 5 is arbitrary
         for n in range(50):
             for j in range(len(M[0,:])-1):
                 for i in range(len(M[:,0])-1):
-                    if M[i][j] != (1|-1):
+                    if M[i][j] != (1|-1|5):
                         M[i,j] = 1/4 * (M[i-1][j] + M[i+1][j] + M[i][j-1] + M[i][j+1])
                     else:
                         pass
